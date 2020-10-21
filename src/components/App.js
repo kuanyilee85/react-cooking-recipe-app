69efb92557d9b10e9d4d3007b1301493fb289aba
kuanyilee85 <kuanyilee85@gmail.com>
2020-10-20 22:58:20 -0400

    add css, font; setup app recipe state; add/ delete function

---
+++ src/components/App.js
@@ -0,0 +1,78 @@
+import React, {useState} from 'react';
+import RecipeList from './RecipeList';
+import '../css/app.css';
+import {v4 as uuidv4} from 'uuid';
+
+function App() {
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(),
+      name: 'New',
+      servings: 1,
+      cookTime: '1:00',
+      instructions: 'Instr.',
+      ingredients: [
+        {id: uuidv4, name: 'Name', amount: '1 Tbs'}
+      ]
+    }
+  
+    setRecipes([...recipes, newRecipe]);
+  }
+
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter(recipe => recipe.id !== id))
+  }
+
+  const [recipes, setRecipes] = useState(sampleRecipes)
+  return (
+    <RecipeList 
+      recipes={recipes}
+      handleRecipeAdd = {handleRecipeAdd}
+      handleRecipeDelete = {handleRecipeDelete}
+      />
+    )
+}
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Plain Chicken',
+    cookTime: '1:45',
+    servings: 3,
+    instructions:
+      '1. Put Salt on chicken\n2. Put chicken in oven\n3. Eat chicken',
+    ingredients: [
+      {
+        id: 1,
+        name: 'chicken',
+        amount: '2 pounds',
+      },
+      {
+        id: 2,
+        name: 'salt',
+        amount: '3tbs',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Plain Pork',
+    cookTime: '10:45',
+    servings: 2,
+    instructions: '1. Put Papaya on pork\n2. Put pork in oven\n3. Eat pork',
+    ingredients: [
+      {
+        id: 1,
+        name: 'pork',
+        amount: '5 pounds',
+      },
+      {
+        id: 2,
+        name: 'pepper',
+        amount: '4tbs',
+      },
+    ],
+  },
+];
+
+export default App;

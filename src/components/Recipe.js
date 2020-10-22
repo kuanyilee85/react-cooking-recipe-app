65613fa36b2bb1f0e2f0b4900abd12ce2c9932d2
kuanyilee85 <kuanyilee85@gmail.com>
2020-10-21 22:25:31 -0400

    style recipeEdit

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,25 +1,24 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react'
 import IngredientList from './IngredientList';
-import {RecipeContext} from './App'
+import { RecipeContext } from './App'
 
 export default function Recipe(props) {
-  const {handleRecipeDelete} = useContext(RecipeContext)
+  const { handleRecipeDelete } = useContext(RecipeContext)
   const {
     id,
-    name, 
-    cookTime, 
-    servings, 
+    name,
+    cookTime,
+    servings,
     instructions,
-    ingredients,
-   } = props
-
+    ingredients
+  } = props
   return (
     <div className="recipe">
       <div className="recipe__header">
         <h3 className="recipe__title">{name}</h3>
         <div>
           <button className="btn btn--primary mr-1">Edit</button>
-          <button 
+          <button
             className="btn btn--danger"
             onClick={() => handleRecipeDelete(id)}
           >
@@ -28,16 +27,16 @@ export default function Recipe(props) {
         </div>
       </div>
       <div className="recipe__row">
-        <span className="recipe__label">Cooking Time:</span>
+        <span className="recipe__label">Cook Time:</span>
         <span className="recipe__value">{cookTime}</span>
       </div>
       <div className="recipe__row">
-        <span className="recipe__label">Serving:</span>
+        <span className="recipe__label">Servings:</span>
         <span className="recipe__value">{servings}</span>
       </div>
       <div className="recipe__row">
         <span className="recipe__label">Instructions:</span>
-        <div className="recipe__value recipe__value--instructions recipe__value--indented">{instructions}</div>
+        <div className="recipe__value recipe__instructions recipe__value--indented">{instructions}</div>
       </div>
       <div className="recipe__row">
         <span className="recipe__label">Ingredients:</span>

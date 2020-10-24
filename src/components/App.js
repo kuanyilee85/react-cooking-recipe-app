a8d31ae6924bb4c60cb723c77a871ef647d7da14
kuanyilee85 <kuanyilee85@gmail.com>
2020-10-23 20:49:53 -0400

    btn functions

--- src/components/App.js
+++ src/components/App.js
@@ -35,19 +35,22 @@ function App() {
   function handleRecipeAdd() {
     const newRecipe = {
       id: uuidv4(),
-      name: 'New',
+      name: '',
       servings: 1,
-      cookTime: '1:00',
-      instructions: 'Instr.',
+      cookTime: '',
+      instructions: '',
       ingredients: [
-        {id: uuidv4, name: 'Name', amount: '1 Tbs'}
+        {id: uuidv4, name: '', amount: ''}
       ]
     }
-  
+    setSelectedRecipeId(newRecipe.id)
     setRecipes([...recipes, newRecipe]);
   }
 
   function handleRecipeDelete(id) {
+    if (selectedRecipeId != null && selectedRecipeId === id) {
+      selectedRecipeId(undefined)
+    }
     setRecipes(recipes.filter(recipe => recipe.id !== id))
   }
 
